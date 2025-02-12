62430f098877e5dbc25ef3f6c71712c3ca549b42
Wsanchez91 <williamsanchez.8055@gmail.com>
2025-02-12 13:41:28 -0500

    update

---
+++ Develop/README2.md
@@ -0,0 +1,29 @@
+# Practice ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+This is to show how node.js works
+
+## Table of Contents
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributors](#contributors)
+[Questions](#questions)
+
+## Installation
+npm i
+        
+## Usage
+to create a README
+
+## License
+The license being used for this project is the MIT license. 
+
+## Contributors
+William Sanchez
+
+## Questions
+My Github username is [Wsanchez91](https://www.github.com/Wsanchez91)
+
+I can be contacted at williamsanchez.8055@gmail.com if you have any questions regarding the project.
--- Develop/index.js
+++ Develop/index.js
@@ -1,67 +1,95 @@
 import inquirer from "inquirer";
 import fs from "fs";
-import generateMarkdown from "./utils/generateMarkdown.js";
 
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Provide a short description of your project:",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What are the installation instructions?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "How do you use this application?",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Choose a license:",
-    choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3-Clause", "None"],
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "How can others contribute to this project?",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Provide test instructions:",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Enter your GitHub username:",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter your email address:",
-  },
-];
+inquirer.prompt([
+    {
+        type: 'input', 
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input', 
+        message: 'What is a description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input', 
+        message: 'What other installations are required to run your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input', 
+        message: 'How is your project used?',
+        name: 'usage',
+    },
+    {
+        type: 'list', 
+        message: 'What license should be used for your project?',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+    },
+    {
+        type: 'input', 
+        message: 'Who contributed to the project?',
+        name: 'contributors',
+    },
+    {
+        type: 'input', 
+        message: 'What is your Github username?',
+        name: 'username',
+    },
+    {
+        type: 'input', 
+        message: 'What is your email?',
+        name: 'email',
+    },
+]).then((res) =>
+    {
+        let readmeText = 
+`# ${res.title} ${renderLicenseBadge(res.license)}
 
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    err ? console.log(err) : console.log("README.md created successfully!");
-  });
-}
+## Description
+${res.description}
 
-function init() {
-  inquirer.prompt(questions).then((answers) => {
-    const readmeContent = generateMarkdown(answers);
-    writeToFile("README.md", readmeContent);
-  });
-}
+## Table of Contents
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributors](#contributors)
+[Questions](#questions)
 
-init();
+## Installation
+${res.installation}
+        
+## Usage
+${res.usage}
+
+## License
+The license being used for this project is the ${res.license} license. 
+
+## Contributors
+${res.contributors}
+
+## Questions
+My Github username is [${res.username}](https://www.github.com/${res.username})
+
+I can be contacted at ${res.email} if you have any questions regarding the project.`;
+
+        fs.writeFile("README2.md", readmeText, (err) => {
+            if (err) console.log(err);
+            else {
+                console.log("README written");
+            }
+        });
+        function renderLicenseBadge(license) {
+            if (license !== 'None') {
+              return `![GitHub license](https://img.shields.io/badge/license-${license.replace(
+                ' ',
+                '_'
+              )}-blue.svg)`;
+            }
+            return '';
+          }
+    }
+  );
